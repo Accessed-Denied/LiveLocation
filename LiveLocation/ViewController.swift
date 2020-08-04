432b86614964a7e703a5561b894f68e6e4729383
Rohit Saini <sketchlearn7@gmail.com>
2020-08-04 16:58:36 +0530

    Initial Commit

---
+++ LiveLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveLocation
+//
+//  Created by Rohit Saini on 04/08/20.
+//  Copyright © 2020 AccessDenied. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
